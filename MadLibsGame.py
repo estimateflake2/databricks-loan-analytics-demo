9de8bf727ce056a9af5ae3765ed2ae53c21ca48b
jbankole <joseph.bankole@gmail.com>
2025-05-22 10:30:16 -0500

    MadLibsGame added

---
+++ MadLibsGame.py
@@ -0,0 +1,7 @@
+color = input ("enter a color: ")
+noun = input ("enter a noun: ")
+celebration = input ("enter a celebration: ")
+
+print("Roses are " + color)
+print(noun+" are blue")
+print("I love "+ celebration)
