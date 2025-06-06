b119c2168756c8f6a43850885ee67761fee8d6b0
estimateflake2 <oladipo.bankole@gmail.com>
2025-06-06 10:19:17 -0500

    Delete MadLibsGame.py

--- MadLibsGame.py
+++
@@ -1,7 +0,0 @@
-color = input ("enter a color: ")
-noun = input ("enter a noun: ")
-celebration = input ("enter a celebration: ")
-
-print("Roses are " + color)
-print(noun+" are blue")
-print("I love "+ celebration)
